4bc452d9d875430fce5f293369791a24bd7930ea
AJ Stubberud <stubberudaj@gmail.com>
2012-10-14 16:01:46 -0700

    Added a button press

--- jakedrive.c
+++ jakedrive.c
@@ -7,20 +7,33 @@
 
 #include "JoystickDriver.c"
 
+#define CONTROLLER_A 01
+
 task main()
 {
   float fmot1,fmot2;    // Declare float variables
   int imot1, imot2;     // Declare integer variables
+  bool slow = false;
 
   while(true)                            // Infinite loop:
   {
     getJoystickSettings(joystick);       // This step is required for the Joystick to work
+
+    if (joy1Btn(CONTROLLER_A)) {
+    	slow = !slow;
+    	continue;
+  	}
 
     fmot2 = (float) joystick.joy1_y1 * 100. / 128.;   // Convert the 1~128 value from the y value of left joystick into 1~100
     fmot1 = (float) joystick.joy1_y2 * 100. / 128.;   // Convert the 1~128 value from the y value of right joystick into 1~100
 
     imot1 = (int) fmot1;   // Convert float into integer for motor
     imot2 = (int) fmot2;
+
+    if (slow) {
+    	imot1 /= 2;
+    	imot2 /= 2;
+  	}
 
     if( imot2 >= -3 && imot2 <= 3 )   // If the motor value is +-3 from 0, it is considered 0
     {
@@ -44,9 +57,6 @@ task main()
       motor[motorE] = imot1;          // Else, the imot1 from line 37 is assigned
       motor[motorD] = imot1;
     }
-
-
-
 
   }
 }
