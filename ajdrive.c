eb23881727eb3286425181cda26ee5600d9d509c
AJ Stubberud <stubberudaj@gmail.com>
2012-10-14 17:02:42 -0700

    Fixed right vs left

--- ajdrive.c
+++ ajdrive.c
@@ -26,10 +26,10 @@ task main()
   	getJoystickSettings(joystick);
 
   	joystick_t joysticks[2]; // 0: Left   1: Right
-    joysticks[0].x = joystick.joy1_x1 * 100.0 / 128.0;
-  	joysticks[0].y = joystick.joy1_y1 * 100.0 / 128.0;
-  	joysticks[1].x = joystick.joy1_x2 * 100.0 / 128.0;
-  	joysticks[1].y = joystick.joy1_y2 * 100.0 / 128.0;
+    joysticks[1].x = joystick.joy1_x1 * 100.0 / 128.0;
+  	joysticks[1].y = joystick.joy1_y1 * 100.0 / 128.0;
+  	joysticks[0].x = joystick.joy1_x2 * 100.0 / 128.0;
+  	joysticks[0].y = joystick.joy1_y2 * 100.0 / 128.0;
 
   	if (joy1Btn(CONTROLLER_A))
   	{
