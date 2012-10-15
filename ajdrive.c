e29d10f11b00ac4f78419b3c3a67e45934fc02fd
AJ Stubberud <stubberudaj@gmail.com>
2012-10-14 22:39:27 -0700

    Conceptually corrected side reversal
    Refer to comments:
    
    Motors D and E are actually on the right side - use joysticks[1]
    F and G are on the left - use joysticks[0]

--- ajdrive.c
+++ ajdrive.c
@@ -25,11 +25,11 @@ task main()
   {
     getJoystickSettings(joystick);
 
-    joystick_t joysticks[2]; // 0: Right   1: Left
-    joysticks[1].x = joystick.joy1_x1 * 100.0 / 128.0;
-    joysticks[1].y = joystick.joy1_y1 * 100.0 / 128.0;
-    joysticks[0].x = joystick.joy1_x2 * 100.0 / 128.0;
-    joysticks[0].y = joystick.joy1_y2 * 100.0 / 128.0;
+    joystick_t joysticks[2]; // 0: Left   1: Right
+    joysticks[0].x = joystick.joy1_x1 * 100.0 / 128.0;
+    joysticks[0].y = joystick.joy1_y1 * 100.0 / 128.0;
+    joysticks[1].x = joystick.joy1_x2 * 100.0 / 128.0;
+    joysticks[1].y = joystick.joy1_y2 * 100.0 / 128.0;
 
     if (joy1Btn(CONTROLLER_A))
     {
@@ -40,9 +40,11 @@ task main()
       movementConstant = 1.0;
     }
 
-    motor[motorD] = joysticks[0].y * movementConstant;
-    motor[motorE] = joysticks[0].y * movementConstant;
-    motor[motorF] = joysticks[1].y * movementConstant;
-    motor[motorG] = joysticks[1].y * movementConstant;
+    // Motors D and E are actually on the right side - use joysticks[1]
+    // F and G are on the left - use joysticks[0]
+    motor[motorD] = joysticks[1].y * movementConstant;
+    motor[motorE] = joysticks[1].y * movementConstant;
+    motor[motorF] = joysticks[0].y * movementConstant;
+    motor[motorG] = joysticks[0].y * movementConstant;
   }
 }
