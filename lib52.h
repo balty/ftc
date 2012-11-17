0d2c4dad885d4a46150a5cf8c81a484f5b566e02
Lori's computer <none@none>
2012-11-16 19:58:13 -0800

    Wrote legodrive, added fine stoppage control to joystick functionality

--- lib52.h
+++ lib52.h
@@ -22,6 +22,14 @@ void updateJoysticks() {
   joysticks[0].y = joystick.joy1_y1 * 100.0 / 128.0;
   joysticks[1].x = joystick.joy1_x2 * 100.0 / 128.0;
   joysticks[1].y = joystick.joy1_y2 * 100.0 / 128.0;
+
+  for (int i = 0; i < 2; i++)
+  {
+  	if (abs(joysticks[i].y) < 5)
+  		joysticks[i].y = 0;
+  	if (abs(joysticks[i].x) < 5)
+  		joysticks[i].x = 0;
+  }
 }
 
 #endif
