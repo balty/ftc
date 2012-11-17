2c021af4ede83cba2f46ff93876f2b58d71008d8
Lori's computer <none@none>
2012-11-16 19:07:20 -0800

    Split joystick functionality into lib52.h

--- lib52.h
+++ lib52.h
@@ -9,4 +9,14 @@ typedef struct {
   int y;
 } joystick_t;
 
-#endif 
+joystick_t joysticks[2]; // 0: left		1: right
+
+void updateJoysticks() {
+	getJoystickSettings(joystick);
+	joysticks[0].x = joystick.joy1_x1 * 100.0 / 128.0;
+  joysticks[0].y = joystick.joy1_y1 * 100.0 / 128.0;
+  joysticks[1].x = joystick.joy1_x2 * 100.0 / 128.0;
+  joysticks[1].y = joystick.joy1_y2 * 100.0 / 128.0;
+}
+
+#endif
