a4dd60f204b084dfb9a91bc51b2a77fbe08f034f
AJ Stubberud <stubberudaj@gmail.com>
2013-01-26 18:07:38 -0800

    Added rawrot.c, a program for testing raw rotation times

---
+++ tests/rawrot.c
@@ -0,0 +1,22 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Sensor, S3,     IRSensor,       sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     gyro,      sensorI2CHiTechnicGyro)
+#pragma config(Motor,  mtr_S1_C1_1,     leftWheels,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     rightWheels,   tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_1,     motorARM1,     tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     motorARM2,     tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Servo,  srvo_S1_C3_1,    clawservo1,           tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_2,    clawservo2,           tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)
+
+#include "../util.h"
+
+task main()
+{
+	rotateLeft();
+	wait1Msec(843);
+	stopMovement();
+}
