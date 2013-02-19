f294ece3e71a031bb8f5a3a8b92361a9849314ba
AJ Stubberud <stubberudaj@gmail.com>
2013-02-18 17:17:02 -0800

    Started sweepdef: sweep then defend

---
+++ sweepdef.c
@@ -0,0 +1,31 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S3,     IRSensor,       sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     IRSensor2,      sensorHiTechnicIRSeeker1200)
+#pragma config(Motor,  mtr_S1_C1_1,     leftWheels,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     rightWheels,   tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_1,     motorARM1,     tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     motorARM2,     tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Servo,  srvo_S1_C3_1,    clawservo1,           tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_2,    clawservo2,           tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_3,    sweeper,              tServoNone)
+#pragma config(Servo,  srvo_S1_C3_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)
+
+#include "util.h"
+
+task main()
+{
+	//waitForStart();
+
+	servo[sweeper] = 169;
+	wait1Msec(1000);
+
+	forward(.6);
+	wait1Msec(600);
+	stopMovement();
+
+	servo[sweeper] = 0;
+	wait1Msec(1000);
+}
